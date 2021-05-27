d5ce19eae7e6b098592dff8919c338344119c910
Maxim Zuyzko <maximzuyzko@gmail.com>
2021-05-27 14:16:10 +0400

    product added

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,12 @@ $(function () {
         fade: true,
         autoplay: true,
         autoplaySpeed: 5000,
+    });
+
+    $('.star').rateYo({
+        starWidth: '17px',
+        normalFill: '#ccccce',
+        rateYo: '#ffc35b',
+        readOnly: true,
     })
 })
